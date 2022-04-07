168e4278c1ee22ef1677d5a2cb7167d384c1d3a9
AbdelrahmanAlaa <84046914+AbdelrahmanAlaa@users.noreply.github.com>
2022-04-07 19:40:16 +0200

    Update authUsersController.js

--- controller/authUsersController.js
+++ controller/authUsersController.js
@@ -7,7 +7,7 @@ const sendEmail = require('./../middleware/email');
 const crypto = require('crypto')
 
 exports.register = asyncError(async (req, res,next) => {
-    const {error} = validateLogin(req.body);
+    const {error} = validateUser(req.body);
     if (error)return res.status(400).json({
         status :"false",
         message :error.details[0].message
