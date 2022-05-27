a4ad7165653fed5c1ef2d3d43a2697c8055b6d04
AbdelrahmanAlaa <84046914+AbdelrahmanAlaa@users.noreply.github.com>
2022-05-27 16:07:25 +0200

    add route of products

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
 // const { limiter } = require('./middleware/limiter');
-const dotenv = require('dotenv')
-const mongoose = require('mongoose');;
-const cors = require('cors');
-const users = require('./routes/userRoutes');
-const products = require('./routes/productsRoutes');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const users = require("./routes/userRoutes");
+const subCategory = require("./routes/subCategoryRoutes");
+const category = require("./routes/categoryRoutes");
+const brand = require("./routes/brandRoutes");
+const product = require("./routes/productRoutes");
 
 dotenv.config({
   path: `${__dirname}/config/.env`,
-})
+});
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.options('*',cors())
+app.options("*", cors());
 
-app.use('/api/users', users);
-app.use('/api/products', products);
+app.use("/api/users", users);
+app.use("/api/subCategory", subCategory);
+app.use("/api/category", category);
+app.use("/api/brands", brand);
+app.use("/api/products", product);
 
+app.all("*", (req, res, next) => {
+  const err = new Error(`can't find this route ${req.originalUrl}`);
+  next(err);
+});
+
+// global error handling
+app.use((err, req, res, next) => {
+  res.status(400).json({ error: err.message });
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`unhandledRejection Errors:${err}`);
+  process.exit(1);
+});
 // app.use('/api/forgetPassword', forgetPassword);
 
-
-mongoose.connect(process.env.CONNECT_DB, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('connected is done ')
-        const port = process.env.PORT || 3030;
-        const server = app.listen(port, () => console.log(`Listening on port http://localhost:${port} ...`));
-    })
-    .catch((err) => { console.log('something is wrong .. ', err) });
-
-
-    
-
-
-
+mongoose
+  .connect(process.env.CONNECT_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected is done ");
+    const port = process.env.PORT || 3030;
+    const server = app.listen(port, () =>
+      console.log(`Listening on port http://localhost:${port} ...`)
+    );
+  });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,37 +1,21 @@
-const express = require('express');
-const authUsers = require('../controller/authUsersController');
+const express = require("express");
+const authUsers = require("../controller/authUsersController");
 const router = express.Router();
-const validate = require('./../middleware/validationError')
+const validate = require("../utils/validation/validationUser");
 
 // routes of review controller
-router
-.route('/register')
-.post(validate.validateRegister,
-    authUsers.register) 
+router.route("/register").post(validate.validateRegister, authUsers.register);
+
+router.route("/login").post(validate.validateLogin, authUsers.login);
+
+router.route("/getAllUsers").get(authUsers.getAllUsers);
+
+router.route("/getUserById/:id").get(authUsers.getUserById);
+
+router.route("/forgetPassword").get(authUsers.forgetPassword);
 
 router
-.route('/login')
-.post(validate.validateLogin,
-    authUsers.login) 
+  .route("/restPassword/:token")
+  .post(validate.validateRestPassword, authUsers.restPassword);
 
-router
-.route('/getAllUsers')
-.get(authUsers.getAllUsers) 
-
-router
-.route('/getUserById/:id')
-.get( authUsers.getUserById) 
-
-router
-.route('/forgetPassword')
-.get(authUsers.forgetPassword) 
-
-router
-.route('/restPassword/:token')
-.post(validate.validateRestPassword,
-    authUsers.restPassword) 
-
-
-
-
-module.exports = router ; 
+module.exports = router;
