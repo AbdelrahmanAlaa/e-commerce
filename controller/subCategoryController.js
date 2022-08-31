1d650d3e8495595fa71ddb2f453d3fbf99d26aae
AbdelrahmanAla <bidoala73@gmail.com>
2022-08-31 18:09:59 +0200

    make it better

--- controller/subCategoryController.js
+++ controller/subCategoryController.js
@@ -17,21 +17,35 @@ exports.getSubCategory = asyncError(async (req, res) => {
   let filterObject = {};
   if (req.params.categoryId) filterObject = { category: req.params.categoryId };
 
-const countDocuments = await SubCategory.countDocuments();
-  const apiFeatures = new ApiFeatures(req.query,SubCategory.find({ filterObject }))
-  .filter()
-  .sort()
-  .search()
-  .paginate(countDocuments)
-  .limitFields();
+  const contDocuments=await SubCategory.countDocuments();
+const apiFeatures = new ApiFeatures(req.query,SubCategory.find({filterObject}))
+.sort()
+.paginate(contDocuments)
+.limitFields()
+.filter()
+.search('Products');
 
-const {mongooseQuery, paginationResult} = apiFeatures;
-const subCategory = await mongooseQuery ; 
+ const {mongooseQuery,paginationResult} = apiFeatures; 
+  const subCategory = await mongooseQuery;
+    if (!subCategory) res.status(404).json({ message: "this id is not found ..! " });
+  
+    res.status(200).json({ result: product.length,paginationResult,subCategory });
+  
+// const countDocuments = await SubCategory.countDocuments();
+//   const apiFeatures = new ApiFeatures(req.query,SubCategory.find({ filterObject }))
+//   .filter()
+//   .sort()
+//   .search()
+//   .paginate(countDocuments)
+//   .limitFields();
 
-  if (!subCategory)
-    res.status(404).json({ message: "this id is not found ..! " });
+// const {mongooseQuery, paginationResult} = apiFeatures;
+// const subCategory = await mongooseQuery ; 
 
-  res.status(200).json({ result: subCategory.length, paginationResult, subCategory });
+  // if (!subCategory)
+  //   res.status(404).json({ message: "this id is not found ..! " });
+
+  // res.status(200).json({ result: subCategory.length, paginationResult, subCategory });
 });
 exports.getSubCategoryByID = asyncError(async (req, res) => {
   const subCategory = await SubCategory.findById(req.params.id);
